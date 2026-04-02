591bd1e56c19e620904e3264f20e4a3142b6408a
nickSwany <Thief.leroy@yandex.ru>
2026-04-02 19:17:29 +0300

    Add app theme, hilt and dependencies

---
+++ app/src/main/java/com/example/nikita_lebedev_interval_timer/ui/theme/Spacing.kt
@@ -0,0 +1,12 @@
+package com.example.nikita_lebedev_interval_timer.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val xs = 4.dp
+    val s = 8.dp
+    val m = 12.dp
+    val l = 16.dp
+    val xl = 20.dp
+    val xxl = 24.dp
+}
